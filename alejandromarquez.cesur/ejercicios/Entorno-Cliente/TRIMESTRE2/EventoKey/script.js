2db32b02c79a85dd6ef0178585034350856c52f6
reddevman <alemarqred@gmail.com>
2021-02-23 20:45:07 +0100

    Actualizado ejercicio keypress

--- alejandromarquez.cesur/ejercicios/Entorno-Cliente/TRIMESTRE2/EventoKey/script.js
+++ alejandromarquez.cesur/ejercicios/Entorno-Cliente/TRIMESTRE2/EventoKey/script.js
@@ -1,4 +1,6 @@
-document.addEventListener("keydown", function (event) {
+var keyTwice = false;
+
+window.addEventListener("keydown", function (event) {
 
     if (event.defaultPrevented) {
         return;
@@ -6,18 +8,48 @@ document.addEventListener("keydown", function (event) {
 
     var key = event.key || event.keyCode;
     if (key === "F1") {
-        document.querySelector("#texto").innerHTML = "F1 pulsada.";
-        if (key) {
-            document.querySelector("#texto").innerHTML = "Se borrará el texto."
-            timeOut();
+        document.querySelector("#texto").innerHTML = "F1 PULSADA. Pulsa F1 para eliminar el mensaje.";
+        if (keyTwice) {
+            document.querySelector("#texto").innerHTML = "";
         }
+        keyTwice = true;
+        setTimeout(function () {
+            keyTwice = 0;
+        }, 1000);
     }
-    
+
     if (key !== "F1") {
         document.querySelector("#texto").innerHTML = "Pulsa F1 para ayuda.";
     }
 });
 
-function timeOut() {
-    setTimeout(function () { document.querySelector("#texto").innerHTML = "" }, 1000);
-}
+// window.addEventListener("keyup", function (event)
+// {
+//     if (event.defaultPrevented) {
+//         return;
+//     }
+
+//     var key = event.key || event.keyCode;
+//     if (key === "F1") {
+//         document.querySelector("#texto").innerHTML = "";
+//     }
+// });
+
+var twice_37 = 0;
+
+$(document).on('keyup', function (e) {
+
+    if (e.which === 37) { // If left arrow    
+
+        if (twice_37 === 1) { // (remember that twice_37 is 0 initially)
+            alert('Do something! (you pressed Left twice!)');
+        }
+
+        twice_37 = 1; // Set to 1 and...
+        setTimeout(function () { // ...reset to 0 after 1s
+            twice_37 = 0;
+        }, 1000);
+
+    }
+
+});
