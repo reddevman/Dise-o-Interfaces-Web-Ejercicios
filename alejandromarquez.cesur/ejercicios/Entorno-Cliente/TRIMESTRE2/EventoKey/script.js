a05cbb172c0ae480ffd4b101298f2b4700ee072e
reddevman <alemarqred@gmail.com>
2021-02-23 19:51:26 +0100

    actualizado ejercicio evento key

--- alejandromarquez.cesur/ejercicios/Entorno-Cliente/TRIMESTRE2/EventoKey/script.js
+++ alejandromarquez.cesur/ejercicios/Entorno-Cliente/TRIMESTRE2/EventoKey/script.js
@@ -6,9 +6,18 @@ document.addEventListener("keydown", function (event) {
 
     var key = event.key || event.keyCode;
     if (key === "F1") {
-        document.querySelector("#texto").innerHTML = "F1 pulsada, pulsa otra tecla para borrar mensaje.";
-    } else {
+        document.querySelector("#texto").innerHTML = "F1 pulsada.";
+        if (key) {
+            document.querySelector("#texto").innerHTML = "Se borrará el texto."
+            timeOut();
+        }
+    }
+    
+    if (key !== "F1") {
         document.querySelector("#texto").innerHTML = "Pulsa F1 para ayuda.";
     }
-
 });
+
+function timeOut() {
+    setTimeout(function () { document.querySelector("#texto").innerHTML = "" }, 1000);
+}
